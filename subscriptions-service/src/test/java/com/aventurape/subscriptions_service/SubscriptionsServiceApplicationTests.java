31a2ac5f0b31450c32236595636f98f3a8c32e3a
Jimena Cama <u202210778@upc.edu.pe>
2025-06-08 22:33:45 -0500

    feat: add subscriptions service

---
+++ subscriptions-service/src/test/java/com/aventurape/subscriptions_service/SubscriptionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aventurape.subscriptions_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
