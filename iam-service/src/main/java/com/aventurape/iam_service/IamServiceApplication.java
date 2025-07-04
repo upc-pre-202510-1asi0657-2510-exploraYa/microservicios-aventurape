ca5f35445b0cc4b30b0d702f92bd1a0f1f5c08aa
aksonie <u202210778@upc.edu.pe>
2025-07-04 18:19:04 -0500

    fix: config

--- iam-service/src/main/java/com/aventurape/iam_service/IamServiceApplication.java
+++ iam-service/src/main/java/com/aventurape/iam_service/IamServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableJpaAuditing
+@EnableJpaRepositories
 public class IamServiceApplication {
 
 	public static void main(String[] args) {
