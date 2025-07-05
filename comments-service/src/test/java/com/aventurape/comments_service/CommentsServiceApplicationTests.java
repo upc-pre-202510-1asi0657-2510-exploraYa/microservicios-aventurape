06184e3ffee9fb17664e3e8f5e33167d9cda69b3
aksonie <u202210778@upc.edu.pe>
2025-07-05 11:59:28 -0500

    fix: gateway

--- comments-service/src/test/java/com/aventurape/comments_service/CommentsServiceApplicationTests.java
+++ comments-service/src/test/java/com/aventurape/comments_service/CommentsServiceApplicationTests.java
@@ -2,12 +2,31 @@ package com.aventurape.comments_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=password",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "eureka.client.enabled=false",
+    "spring.rabbitmq.host=localhost",
+    "spring.rabbitmq.port=5672",
+    "spring.rabbitmq.username=guest",
+    "spring.rabbitmq.password=guest",
+    "spring.cloud.config.enabled=false",
+    "authorization.jwt.secret=AventuraPeSecureTokenKey2025WithStrongSecurityRequiredFor256Bits",
+    "authorization.jwt.expiration.days=7",
+    "post-service.ribbon.listOfServers=localhost:8083",
+    "documentation.application.description=Test Description",
+    "documentation.application.version=0.0.1-SNAPSHOT"
+})
 class CommentsServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
--- subscriptions-service/src/test/java/com/aventurape/subscriptions_service/SubscriptionsServiceApplicationTests.java
+++ subscriptions-service/src/test/java/com/aventurape/subscriptions_service/SubscriptionsServiceApplicationTests.java
@@ -2,12 +2,28 @@ package com.aventurape.subscriptions_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=password",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "eureka.client.enabled=false",
+    "spring.rabbitmq.host=localhost",
+    "spring.rabbitmq.port=5672",
+    "spring.rabbitmq.username=guest",
+    "spring.rabbitmq.password=guest",
+    "spring.cloud.config.enabled=false",
+    "authorization.jwt.secret=AventuraPeSecureTokenKey2025WithStrongSecurityRequiredFor256Bits",
+    "authorization.jwt.expiration.days=7"
+})
 class SubscriptionsServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
