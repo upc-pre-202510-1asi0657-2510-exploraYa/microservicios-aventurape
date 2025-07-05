cfbc3b2ef5910744fc640aa059cf03ccc739f86d
aksonie <u202210778@upc.edu.pe>
2025-07-05 00:27:50 -0500

    fix: favorites in config

--- favorites-service/src/test/java/com/aventurape/favorites_service/FavoritesServiceApplicationTests.java
+++ favorites-service/src/test/java/com/aventurape/favorites_service/FavoritesServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.aventurape.favorites_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FavoritesServiceApplicationTests {
 
 	@Test
--- stats-service/src/test/java/com/aventurape/stats_service/StatsServiceApplicationTests.java
+++ stats-service/src/test/java/com/aventurape/stats_service/StatsServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.aventurape.stats_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class StatsServiceApplicationTests {
 
 	@Test
